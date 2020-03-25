a7207003d56da7f3be26221753f64d4125e4ef01
brumadu <madureira12.bm@gmail.com>
2020-03-25 17:22:04 -0300

    Feat: linked thumbnails

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
 
-let xhr = new XMLHttpRequest();
-xhr.onreadystatechange = addVideo;
-xhr.open("GET", "http://localhost:9000/videos");
+let xhrVideos = new XMLHttpRequest();
+xhrVideos.onreadystatechange = addVideo;
+xhrVideos.open("GET", "http://localhost:9000/videos");
 
 function addVideo() {
     if (this.readyState == 4 && this.status == 200) {
         let response = JSON.parse(this.responseText);
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < 4; i++) {
             let textVideo = response[i];
             if (textVideo.isLastVideo == true) {
-                console.log(textVideo)
+                // console.log(textVideo);
                 document.getElementById("video").src = 'https://www.youtube.com/embed/' + textVideo.id;
+            } else {
+                // console.log(textVideo.thumb);
+                // console.log(textVideo.url);
+                document.getElementById("thumb" + String(i)).src = textVideo.thumb;
+                document.getElementById("linkT" + String(i)).href = textVideo.url;
             }
-            
         }
     }
-
-
-
-
 }
-xhr.send();
 
+xhrVideos.send();
 
+let xhrInsta = new XMLHttpRequest();
+xhrInsta.onreadystatechange = addInsta;
+xhrInsta.open("GET", "http://localhost:9000/instagram");
 
+function addInsta(){
+    if (this.readyState == 4 && this.status == 200){
+        // console.log((this.response));
+
+        let encodedData = btoa(this.response);
+        console.log(encodedData);
+        //     let img = new Image();
+		// 	let response = xhr.responseText;
+        //     let binary = ""
+            
+        //     for(i=0; i<response.length; i++){
+		// 		binary += String.fromCharCode(response.charCodeAt(i) & 0xff);
+		// 	}
+			
+		// 	img.src = 'data:image/jpeg;base64,' + btoa(binary);
+		// 	let canvas = document.getElementById('showImage');
+		// 	let context = canvas.getContext('2d');
+				
+		// 	context.drawImage(img,0,0);
+		// 	let snapshot = canvas.toDataURL("image/png");
+		// 	let twinImage = document.getElementById('twinImg');
+		// 	twinImage.src = snapshot;
+
+    }
+}
+
+// xhr.overrideMimeType('text/plain; charset=x-user-defined');
+xhrInsta.send();
