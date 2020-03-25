5a6aa784ff557ce3cd4ca27eeefbd035c428d3c8
unknown <ian0198@gmail.com>
2020-03-25 17:51:27 -0300

    Feat: getting last instagram post

--- script.js
+++ script.js
@@ -9,11 +9,8 @@ function addVideo() {
         for (let i = 0; i < 4; i++) {
             let textVideo = response[i];
             if (textVideo.isLastVideo == true) {
-                // console.log(textVideo);
                 document.getElementById("video").src = 'https://www.youtube.com/embed/' + textVideo.id;
             } else {
-                // console.log(textVideo.thumb);
-                // console.log(textVideo.url);
                 document.getElementById("thumb" + String(i)).src = textVideo.thumb;
                 document.getElementById("linkT" + String(i)).href = textVideo.url;
             }
@@ -26,32 +23,16 @@ xhrVideos.send();
 let xhrInsta = new XMLHttpRequest();
 xhrInsta.onreadystatechange = addInsta;
 xhrInsta.open("GET", "http://localhost:9000/instagram");
+xhrInsta.overrideMimeType('text/plain; charset=x-user-defined');
+xhrInsta.send();
 
-function addInsta(){
-    if (this.readyState == 4 && this.status == 200){
-        // console.log((this.response));
-
-        let encodedData = btoa(this.response);
-        console.log(encodedData);
-        //     let img = new Image();
-		// 	let response = xhr.responseText;
-        //     let binary = ""
-            
-        //     for(i=0; i<response.length; i++){
-		// 		binary += String.fromCharCode(response.charCodeAt(i) & 0xff);
-		// 	}
-			
-		// 	img.src = 'data:image/jpeg;base64,' + btoa(binary);
-		// 	let canvas = document.getElementById('showImage');
-		// 	let context = canvas.getContext('2d');
-				
-		// 	context.drawImage(img,0,0);
-		// 	let snapshot = canvas.toDataURL("image/png");
-		// 	let twinImage = document.getElementById('twinImg');
-		// 	twinImage.src = snapshot;
-
+function addInsta() {
+    if (this.readyState === 4 && this.status == 200) {
+        var response = xhrInsta.response;
+        var binary = ""
+        for (i = 0; i < response.length; i++) 
+            binary += String.fromCharCode(response.charCodeAt(i) & 0xff);
+    
+        document.getElementById("instagram").src = 'data:image/jpeg;base64,' + btoa(binary)
     }
 }
-
-// xhr.overrideMimeType('text/plain; charset=x-user-defined');
-xhrInsta.send();
