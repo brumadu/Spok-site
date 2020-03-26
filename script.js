4588849f17255db41e83c3777c36a22c19a915c6
brumadu <madureira12.bm@gmail.com>
2020-03-25 21:31:07 -0300

    Feat: Instagram integration// Refactor: Twitter embed

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ xhrVideos.open("GET", "http://localhost:9000/videos");
 function addVideo() {
     if (this.readyState == 4 && this.status == 200) {
         let response = JSON.parse(this.responseText);
-        for (let i = 0; i < 4; i++) {
+        for (let i = 0; i < 3; i++) {
             let textVideo = response[i];
             if (textVideo.isLastVideo == true) {
                 document.getElementById("video").src = 'https://www.youtube.com/embed/' + textVideo.id;
